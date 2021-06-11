23ea5ca4a8efcfdf3610fe2c84151e94e4b28a54
John Andrew Purio <lyerdestroyer@gmail.com>
2021-06-11 18:05:51 +0800

    Draft Commit

---
+++ src/components/Filter/index.jsx
@@ -0,0 +1,43 @@
+import './index.css'
+import { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { selectCurrentGenre, searchRestaurantData } from '../../redux/actions'
+
+export default function Filter() {
+    const genres = useSelector(state => state.genres)
+    const currentGenre = useSelector(state => state.currentGenre)
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+
+    }, [currentGenre] )
+
+    useEffect(() => {
+
+    }, [searchQuery] )
+
+    const selectGenre = (e) => {
+        dispatch( selectCurrentGenre(e.target.value) )
+    }
+
+    const searchHandler = (e) => {
+        dispatch( searchRestaurantData(e.target.value) )
+    }
+
+    return (
+        <div className="filters">
+            <div className="search">
+                <label htmlFor="search">Search</label>
+                <input type="text" name="search" placeholder="Enter the name of your query" onChange={searchHandler} />
+            </div>
+            <div className="select-genre">
+                <label htmlFor="selectedGenre">Select Genre:</label>
+                <select name="selectedGenre" id="select-genre" onChange={selectGenre} >
+                    {
+                        genres.map( (genre, index) => <option key={genre} value={index} >{genre}</option>)
+                    }
+                </select>
+            </div>
+        </div>
+    )
+}
